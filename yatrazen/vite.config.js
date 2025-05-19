a84923754880b46f00cd4c2f4c8448a70363a32e
Sai Dangade <sai3dangade@gmail.com>
2025-05-19 09:22:00 +0530

    minor fixes in import path

--- yatrazen/vite.config.js
+++ yatrazen/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "service": path.resolve(__dirname, "./src/service"),
     },
   },
   server: {
